a7ef48414eb6d9bc6111682942f62c3b0342e3b9
PoojaPatil-Cosmos <104497179+PoojaPatil-Cosmos@users.noreply.github.com>
2025-09-28 10:54:34 +0000

    custom github actions added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hello-pooja
+uses: actions/hello-pooja@e76147da8e5c81eaf017dede5645551d4b94427b
+with:
+  who-to-greet: 'Mona the Octocat'
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 const name = core.getInput('name');
 const output_value = `Hello ${name} from Pooja!`;
 core.setOutput('greeting', output_value);
