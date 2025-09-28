586691320db20d6b0c24abec36535e4fcee7bdb6
PoojaPatil-Cosmos <104497179+PoojaPatil-Cosmos@users.noreply.github.com>
2025-09-28 11:02:47 +0000

    changed json file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
-import * as core from '@actions/github';
+import * as github from '@actions/github';
+
 const name = core.getInput('name');
 const output_value = `Hello ${name} from Pooja!`;
 core.setOutput('greeting', output_value);
